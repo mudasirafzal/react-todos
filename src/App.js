22ca78e0013d87af5785119b2df955015a38eabb
Mstechlabs <mstechlabs@Mstechlabss-MacBook-Air.local>
2020-02-11 01:09:23 +0400

    Added new todos

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
-import React, { Component } from 'react';
-import Todos from './Todos'
-import AddTodo from './AddTodo'
+import React, { Component } from "react";
+import Todos from "./Todos";
+import AddTodo from "./AddTodo";
 
 class App extends Component {
   state = {
     todos: [
-      {id: 1, content: 'buy some milk'},
-      {id: 2, content: 'play mario kart'}
+      { id: 1, content: "Make credit card payment" },
+      { id: 2, content: "Book an appointment" },
+      { id: 3, content: "Get car service done" },
+      { id: 4, content: "Check the project status" },
+      { id: 5, content: "Sales followup" }
     ]
-  }
-  deleteTodo = (id) => {
+  };
+  deleteTodo = id => {
     const todos = this.state.todos.filter(todo => {
-      return todo.id !== id
+      return todo.id !== id;
     });
     this.setState({
       todos
     });
-  }
-  addTodo = (todo) => {
+  };
+  addTodo = todo => {
     todo.id = Math.random();
     let todos = [...this.state.todos, todo];
     this.setState({
       todos
     });
-  }
+  };
   render() {
     return (
       <div className="todo-app container">
