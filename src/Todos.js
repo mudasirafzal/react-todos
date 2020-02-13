1e6a709312a6cc6e6c5fc057511f83f05fa5932b
Mstechlabs <mstechlabs@Mstechlabss-MacBook-Air.local>
2020-02-13 15:13:00 +0400

    Added tick

--- src/Todos.js
+++ src/Todos.js
@@ -1,24 +1,27 @@
-import React from 'react';
+import React from "react";
 
-const Todos = ({todos, deleteTodo}) => {
-
+const Todos = ({ todos, deleteTodo }) => {
   const todoList = todos.length ? (
     todos.map(todo => {
       return (
         <div className="collection-item" key={todo.id}>
-          <span onClick={() => {deleteTodo(todo.id)}}>{todo.content}</span>
+          <span>{todo.content}</span>
+          <span
+            className="fa fa-times done"
+            onClick={() => {
+              deleteTodo(todo.id);
+            }}
+          >
+            {" "}
+          </span>
         </div>
-      )
+      );
     })
   ) : (
     <p className="center">You have no todo's left, yay!</p>
   );
 
-  return (
-    <div className="todos collection">
-      {todoList}
-    </div>
-  )
-}
+  return <div className="todos collection">{todoList}</div>;
+};
 
 export default Todos;
